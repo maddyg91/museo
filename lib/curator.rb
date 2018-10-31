24bbca98f21e82f334ea4ce36b00912ab209f516
Maddie Jones <maddyg91@gmail.com>
2018-10-31 11:05:33 -0600

    working on fine multiple photograph

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,4 +1,5 @@
 require './lib/photograph'
+require './lib/artist'
 require 'pry'
 
 class Curator
@@ -17,12 +18,43 @@ class Curator
     @artists << artist
   end
 
+  def find_artist_by_id(id)
+    @artists.find_all do |artist|
+      artist.id == id
+      end
+  end
+
+  def find_photograph_by_id(id)
+    @photographs.find_all do |photograph|
+      photograph.id == id
+    end
+  end
+
+  def find_photographs_by_artist(artist)
+    @photographs.find_all do |photograph|
+      photograph.artist_id == artist
+    end
+  end
+
 end
-
-
-# pry(main)> curator.find_artist_by_id("1")
-# #=> #<Artist:0x00007fd3a02a8890 @born="1908", @country="France", @died="2004", @id="1", @name="Henri Cartier-Bresson">
 #
-# pry(main)> curator.find_photograph_by_id("2")
-# #=> #<Photograph:0x00007fd3a1801278 @artist_id="2", @id="2", @name="Moonrise, Hernandez", @year="1941">
-# ```
+# pry(main)> diane_arbus = curator.find_artist_by_id("3")
+# #=> #<Artist:0x00007fa3c13eef88 @born="1923", @country="United States", @died="1971", @id="3", @name="Diane Arbus">
+#
+# pry(main)> curator.find_photographs_by_artist(diane_arbus)
+# #=> [#<Photograph:0x00007fa3c13a4050...>, #<Photograph:0x00007fa3c110e4a8...>]
+#
+# pry(main)> curator.artists_with_multiple_photographs
+# #=> [#<Artist:0x00007fa3c13eef88 @born="1923", @country="United States", @died="1971", @id="3", @name="Diane Arbus">]
+#
+# pry(main)> curator.artists_with_multiple_photographs.length
+# #=> 1
+#
+# pry(main)> diane_arbus == curator.artists_with_multiple_photographs.first
+# #=> true
+#
+# pry(main)> curator.photographs_taken_by_artists_from("United States")
+# #=> [#<Photograph:0x00007fa3c286b088...>, #<Photograph:0x00007fa3c13a4050...>, #<Photograph:0x00007fa3c110e4a8...>]
+#
+# pry(main)> curator.photographs_taken_by_artists_from("Argentina")
+# #=> []
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -19,6 +19,19 @@ class CuratorTest < Minitest::Test
       artist_id: "2",
       year: "1941"
     })
+    @photo_3 = Photograph.new ({
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    })
+
+    @photo_4 =Photograph.new ({
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    })
 
     @artist_1 = Artist.new ({
       id: "1",
@@ -33,6 +46,13 @@ class CuratorTest < Minitest::Test
       name: "Ansel Adams",
       born: "1902",
       died: "1984",
+      country: "United States"
+    })
+    @artist_3 = Artist.new ({
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
       country: "United States"
     })
 
@@ -57,11 +77,44 @@ class CuratorTest < Minitest::Test
     assert_equal @photo_1, @curator.photographs.first
     assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", @curator.photographs.first.name
   end
+
   def test_it_can_add_artists
     @curator.add_artist(@artist_1)
     @curator.add_artist(@artist_2)
     assert_equal [@artist_1, @artist_2], @curator.artists
     assert_equal @artist_1, @curator.artists.first
-    assert_equal "Henri Cartier-Bresson", @curator.artists.first
+    assert_equal "Henri Cartier-Bresson", @curator.artists.first.name
+  end
+
+  def test_it_can_find_artist_by_id
+    @curator.add_artist(@artist_2)
+    assert_equal @artist_2, @curator.find_artist_by_id("2")
+  end
+
+  def test_it_can_find_photoraph_by_id
+    @curator.add_photograph(@photo_1)
+    assert_equal @photo_1, @curator.find_photograph_by_id("1")
+  end
+  def test_it_can_find_artist_by_id
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+    diane_arbus = @curator.find_artist_by_id("3")
+    assert_equal [@photo_3, @photo_4],@curator.find_photographs_by_artist(diane_arbus)
+  end
+
+  def test_it_can_find_artist_with_miltiple_photographs
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+    assert_equal [@artist_1], @curator.artists_with_multiple_photographs
   end
 end
